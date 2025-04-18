4a2fb4c49f9c1d8814343cd1e6b420a17310a797
Nicolas Germeau <19894635+ngermeau@users.noreply.github.com>
2025-04-18 14:45:16 +0200

    adding images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
 <script>
-	import Hero from '$lib/components/Hero.svelte';
-	import MoviesList from '$lib/components/MoviesList.svelte';
-	import movies from '$lib/data/movies.json';
-	let IMG_STORAGE_URL = 'https://f005.backblazeb2.com/file/watchbeforeyoudie/';
+  import Hero from "$lib/components/Hero.svelte";
+  import MoviesList from "$lib/components/MoviesList.svelte";
+  import movies from "$lib/data/movies.json";
+  let IMG_STORAGE_URL = "images/"; //local storage, replace for remote storage
 </script>
 
 <main>
-	<div
-		class="bg-background text-text font-poppins grid auto-rows-[400px] grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-1 overflow-hidden text-sm"
-	>
-		<Hero />
-		<MoviesList {movies} imgStorageUrl={IMG_STORAGE_URL} />
-	</div>
+  <div
+    class="bg-background text-text font-poppins grid auto-rows-[400px] grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-1 overflow-hidden text-sm"
+  >
+    <Hero />
+    <MoviesList {movies} imgStorageUrl={IMG_STORAGE_URL} />
+  </div>
 </main>
