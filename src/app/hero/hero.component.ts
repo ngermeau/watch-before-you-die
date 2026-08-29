6f39c3f61cb4ec6ef5a6feb60f42440247ee82c2
Nicolas Germeau <nicolas@mac.home>
2026-08-29 10:50:25 +0200

    updating hero visibility by using signals

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -7,9 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css',
 })
 export class HeroComponent {
-  isVisible: boolean = true;
+  isVisible = signal(true);
 
   toggleVisibility() {
-    this.isVisible = !this.isVisible;
+    this.isVisible.update((value) => !value);
   }
 }
